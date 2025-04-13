19f9fa4fd922054aae0b03013f38156090a3fbdf
HP <dhiachabane@yahoo.fr>
2025-04-13 16:38:39 +0100

    minor bugs fixes

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: 'immersivedigitaldevelopment.com',
+    host: 'https://immersivedigitaldevelopment.com',
   },
   plugins: [
     react(),
